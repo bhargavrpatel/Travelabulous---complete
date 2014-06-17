7b52d367e5cca4a32b496257dfc72b8eb62f2f41
Bhargav Patel <ziikutv@gmail.com>
2014-06-16 21:54:36 -0400

    Finished Questionnaire subsystem

--- travelabulous/assets/js/questionnaire.js
+++ travelabulous/assets/js/questionnaire.js
@@ -1,35 +1,38 @@
 var questionArray = [];
-var optionsArray =[];
+var optionsArray = [];
 var answersArray = [];
 var currentIndex = 0;
 var doneQuestionnaire = false;
 
-function fillOptions(option, boolx){
-  if (boolx == true) {
-    optionsArray.push(option);
-  } else {
-    optionsArray.push(option);
+function fillOptions(option){
+  optionsArray.push(option);
+}
+
+function newArray() {
+  var internalArray = [];
+  if (optionsArray.length == 2) {
+    internalArray = optionsArray.slice(0);
+    optionsArray = [];
+    return internalArray;
   }
 }
 
 function fillQuestions(question) {
     var innerArray = [];
     innerArray.push(question);
-    innerArray.push(optionsArray);
+    innerArray.push(newArray());
     questionArray.push(innerArray);
 }
 
 
-function collectAnswers(leftSelected) {
-    if (leftSelected) {
-      answersArray[currentIndex] = 1;
-    } else {
-      answersArray[currentIndex] = 2;
-    }
+function collectAnswers(number) {
+    answersArray[currentIndex] = number;
 
     if ((currentIndex+1) < questionArray.length) {
       currentIndex += 1;
+      rendering();
     } else {
       doneQuestionnaire = true;
+      rendering();
     }
 }
